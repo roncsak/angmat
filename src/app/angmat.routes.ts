26d88b8e0ce449205651b5d155bdfba9dc498e5b
Tamás Roncsák <tamas@roncsak.hu>
2016-06-22 13:51:10 +0200

    angular components updated to rc3 sidenav layout is fullscreen

--- src/app/angmat.routes.ts
+++ src/app/angmat.routes.ts
@@ -1,10 +1,10 @@
 import { provideRouter, RouterConfig } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent }     from './dashboard/dashboard.component';
 import { MotorsByManufComponent } from './motors-by-manuf/motors-by-manuf.component';
 
 export const routes: RouterConfig = [
-  { path: '/',  component: DashboardComponent  },
-  { path: '/:id',  component: MotorsByManufComponent }
+  { path: '',     component: DashboardComponent  },
+  { path: ':id',  component: MotorsByManufComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
