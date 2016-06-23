2b9382b6c8e35cac15b6a867132472c4da6c0756
Tamás Roncsák <tamas@roncsak.hu>
2016-06-23 13:56:02 +0200

    add initial motor manage component

---
+++ src/app/manage-motor/manage-motor.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-manage-motor',
+  templateUrl: 'manage-motor.component.html',
+  styleUrls: ['manage-motor.component.css']
+})
+export class ManageMotorComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
